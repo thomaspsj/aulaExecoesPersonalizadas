c96f161e47fc87b8d5a4b211fba199535a4920ea
Thomas Paulo Jr <thomaspaulo@hotmail.com>
2024-06-24 22:32:23 -0300

    first commit

---
+++ execoesPersonalizada/Entities/Reservation.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace execoesPersonalizada.Entities
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation()
+        {
+        }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkout)
+        {
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkout;
+        }
+
+        public double Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        public void UpdatesDates(DateTime checkin, DateTime checkOut)
+        {
+            CheckIn = checkin;
+            CheckOut = checkOut;
+        }
+
+        public override string ToString()
+        {
+            return "Room "
+                + RoomNumber
+                + ", check-in: "
+                + CheckIn.ToString("dd/MM/yyyy")
+                + ", check-out: "
+                + CheckOut.ToString("dd/MM/yyyy")
+                + ", "
+                + Duration()
+                + " nigths";
+        }
+    }
+}
---
+++ execoesPersonalizada/Program.cs
@@ -0,0 +1,55 @@
+﻿using execoesPersonalizada.Entities;
+
+namespace execoesPersonalizada 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Room number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Chek-in date (dd/MM/yyyy): ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("Chek-out date (dd/MM/yyyy): ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+            if( checkOut <= checkIn)
+            {
+                Console.WriteLine("Error in resevation:  Check-out date must be after check-in date.");
+            }
+            else
+            {
+                Reservation reservation = new Reservation(number, checkIn, checkOut);
+                Console.WriteLine("Resevertion: " + reservation);
+
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation: ");
+                Console.Write("Chek-in date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Chek-out date (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                DateTime now = DateTime.Now;
+                if ( checkIn < now || checkOut < now)
+                {
+                    Console.WriteLine("Error in reservation: Reservation dates for updates must be future dates.");
+                }
+                else if(checkOut <= checkIn)
+            {
+                    Console.WriteLine("Error in resevation:  Check-out date must be after check-in date.");
+                }
+                else
+                {
+                    reservation.UpdatesDates(checkIn, checkOut);
+                    Console.WriteLine("Reservation: " + reservation);
+
+                }
+
+
+            }
+
+         ;
+
+        }
+    }
+}
